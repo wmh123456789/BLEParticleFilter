34563f227905e48e40692df9da7e64c6197cc531
wmh123456789 <wmh123456789@outlook.com>
2014-12-15 17:05:37 +0800

    singnal data model

--- ProbMap.py
+++ ProbMap.py
@@ -6,16 +6,23 @@ class ProbMap(object):
 		super(ProbMap, self).__init__()
 		self.arg = arg
 		self.SPList = {}
+		self.KeyList = {} # the key of the source, MacAddr or Major-Minor
 
 
 	def LoadSingnalPoint(self,SP):
 		self.SPList.udpate({SP.name:SP})
 		pass
 
+	# Update the Keylist by SPlist
 	def MacFilter(self,MacList):
 		for name in self.SPList:
 			SP = self.SPList[name]
-			
+			for key in SP: # key is macaddr in android
+				if key in self.KeyList:
+					pass
+				else:
+					pass
+
 		pass
 
 
